290355bdcc47a8534b1a156c8d40a1b7ca18fd6c
Okan Torun <okantorun1999@hotmail.com>
2021-10-18 12:08:32 +0300

    Add files via upload

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager:BaseCustomerManager
+    {
+       private ICustomerCheckService customerCheckService;
+
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            this.customerCheckService = customerCheckService;
+        }
+
+        public override void Save(Customer customer)
+        {
+            if (customerCheckService.CheckIfRealPerson(customer))
+            {
+                   base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid person");
+            }
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1999, 5, 15), FirstName = "Okan", LastName = "Torun", NationalityId = "10849914994" });
+        }
+
+
+
+    }
+}
